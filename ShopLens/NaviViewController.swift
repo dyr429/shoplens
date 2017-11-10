0cd7e0e3efd5c47f90c3ca03740896a67c7263b2
Yiren Ding <ding.yir@husky.neu.edu>
2017-11-10 11:09:16 -0500

    update navi function ar feature

--- ShopLens/NaviViewController.swift
+++ ShopLens/NaviViewController.swift
@@ -12,6 +12,10 @@ import ARKit
 
 class NaviViewController: UIViewController, ARSCNViewDelegate {
     
+    @IBOutlet weak var picture2: UIImageView!
+    @IBOutlet weak var picture1: UIImageView!
+    @IBOutlet weak var deleteP2: UIButton!
+    @IBOutlet weak var deleteP1: UIButton!
     @IBOutlet weak var sceneView: ARSCNView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,10 +24,8 @@ class NaviViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-    
+        sceneView.showsStatistics = false
         
-    
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
         let arrow = scene.rootNode.childNodes.first
         arrow!.position = SCNVector3Make(0, -5, -10)
@@ -31,15 +33,15 @@ class NaviViewController: UIViewController, ARSCNViewDelegate {
         
         let position = SCNVector3Make(0, -1.5, -4)
         let scale = SCNVector3Make(1, 1, 1)
-        let textNode = textIndicator(position: position, scale: scale)
+        let textNode = textIndicator(position: position, scale: scale,content: "20m")
 
         scene.rootNode.addChildNode(textNode)
         sceneView.scene = scene
     }
     
-    func textIndicator(position: SCNVector3, scale: SCNVector3)->SCNNode{
-        let text = SCNText(string: "50m", extrusionDepth: 0)
-        text.firstMaterial?.diffuse.contents = UIColor.white
+    func textIndicator(position: SCNVector3, scale: SCNVector3, content: String)->SCNNode{
+        let text = SCNText(string: content, extrusionDepth: 0)
+        text.firstMaterial?.diffuse.contents = UIColor.green
         text.font = UIFont(name: "Helvetica", size: 0.4)
         let textNode = SCNNode()
         textNode.position = position
@@ -94,5 +96,29 @@ class NaviViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    
+    @IBAction func deletePicture1(_ sender: UIButton) {
+        picture1.alpha = 0
+        deleteP1.alpha = 0
+        
+        let scene = SCNScene(named: "art.scnassets/ship2.scn")!
+        let arrow = scene.rootNode.childNodes.first
+        arrow!.position = SCNVector3Make(2, -5, -10)
+        arrow!.scale = SCNVector3Make(1, 1, 1)
+        
+        let position = SCNVector3Make(0, -1.5, -4)
+        let scale = SCNVector3Make(1, 1, 1)
+        let textNode = textIndicator(position: position, scale: scale, content: "10m")
+        
+        
+        scene.rootNode.addChildNode(textNode)
+        sceneView.scene = scene
+       
+    }
+    
+    @IBAction func deletePicture2(_ sender: UIButton) {
+        picture2.alpha = 0
+        deleteP2.alpha = 0
     }
 }
